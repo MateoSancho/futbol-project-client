f7185e646667be74768d93922e78e0408f0ba061
Mateo Sancho <mat.sancho.e@gmail.com>
2025-12-01 10:05:21 +0100

    Error page, with no css

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,18 @@ import { Link } from "react-router-dom";
 function Error () {
     return(
         <div>
-            <h1>Error page</h1>
+            <h1>404 - Page Not Found</h1>
+
+            <p>The page you're looking for can't be found.</p>
+
+            <p>This might have happened because:</p>
+        
+            <ul>
+                <li>The page was moved or deleted</li>
+                <li>You typed the wrong URL</li>
+                <li>The link you clicked is broken</li>
+                <li>The page is temporarily unavailable</li>
+            </ul>
 
             <Link to="/" className="Link">← Back to Home</Link>
         </div>
